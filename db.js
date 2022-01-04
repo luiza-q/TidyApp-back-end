c87cc9bab456ccc0f9fd95cd0c808d9a253e24e2
Elyamira <47004320+Elyamira@users.noreply.github.com>
2022-01-04 10:27:10 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,21 +1,23 @@
 const Pool = require("pg").Pool;
-require("dotenv").config();
+const secrets= require("./db_settings.json")
+// require("dotenv").config();
 
 
-const devConfig = { 
-    user: process.env.PG_USER,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT
-};
+// const devConfig = { 
+//     user: process.env.PG_USER,
+//     database: process.env.PG_DATABASE,
+//     password: process.env.PG_PASSWORD,
+//     host: process.env.PG_HOST,
+//     port: process.env.PG_PORT
+// };
 
-const proConfig = {
-    connectionString: process.env.DATABASE_URL
-}
+// const proConfig = {
+//     connectionString: process.env.DATABASE_URL
+// }
 
-const pool = new Pool(
-    process.env.NODE_ENV === "production" ? proConfig : devConfig
-);
+// const pool = new Pool(
+//     process.env.NODE_ENV === "production" ? proConfig : devConfig
+// );
+const connection = new Pool(secrets);
 
-module.export = pool;
+module.export = connection;
